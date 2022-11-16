5bf5809570bbe1822c475858bef762ea9a76f04d
unknown <2016charanjit@gmail.com>
2022-11-16 16:26:41 +0000

    done

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -4,73 +4,92 @@ import Pic from "./pic";
 import Main from "./main";
 
 const Background = styled.div`
-width: 100%;
-height: 100%;
-background: rgba(0, 0, 0, 0.8);
-position: fixed;
-display: flex;
-justify-content: center;
-align-items: center;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.8);
+  position: fixed;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 const ModalWrapper = styled.div`
-width: 1024px;
-height: 860px;
-box-shadow: 0 5px 16px rgba(0, 0, 0, 0.2);
-background: #fff;
-color: #000;
-display: grid;
-grid-template-columns: 1fr 1fr;
-position: relative;
-z-index: 10;
-border-radius: 10px;
-overflow: scroll;
+  width: 1024px;
+  height: 860px;
+  box-shadow: 0 5px 16px rgba(0, 0, 0, 0.2);
+  background: #fff;
+  color: #000;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  position: relative;
+  z-index: 10;
+  border-radius: 10px;
+  overflow: scroll;
 `;
 const ModalContent = styled.div`
-display: flex;
-flex-direction: column;
-justify-content: center;
-align-items: center;
-line-height: 1.8;
-color: #141414;
-p {
-margin-bottom: 1rem;
-}
-button {
-padding: 10px 24px;
-background: #141414;
-color: #fff;
-border: none;
-}
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  line-height: 1.8;
+  color: #141414;
+  p {
+    margin-bottom: 1rem;
+  }
+  button {
+    padding: 10px 24px;
+    background: #141414;
+    color: #fff;
+    border: none;
+  }
 `;
 const CloseModalButton = styled.button`
-cursor: pointer;
-position: absolute;
-top: 20px;
-right: 20px;
-width: 32px;
-padding: 0;
-z-index: 10;
+  cursor: pointer;
+  position: absolute;
+  top: 20px;
+  right: 20px;
+  width: 32px;
+  padding: 0;
+  z-index: 10;
 `;
 
-export const Modal = ({ showModal, setShowModal, pics, showIndexs, handleShowIndexs }) => {
-return (
-<>
-{showModal ? (
-<Background>
-<ModalWrapper showModal={showModal}>
-<ModalContent>
-<h1> Preferences</h1>
-{pics.map((pic, i) => <Pic pic={pic} key={pic.id} idx={i} handleShowIndexs={handleShowIndexs} showPic={showIndexs.indexOf(i) > -1} modal />)}
-<button> chose</button>
-<CloseModalButton
-aria-label="Close modal"
-onClick={() => setShowModal((prev) => !prev)}
-/>
-</ModalContent>
-</ModalWrapper>
-</Background>
-) : null}
-</>
-);
+export const Modal = ({
+  showModal,
+  setShowModal,
+  pics,
+  showIndexs,
+  handleShowIndexs,
+  handleSubmit,
+  pic,
+}) => {
+  return (
+    <>
+      {showModal ? (
+        <Background>
+          <ModalWrapper showModal={showModal}>
+            <ModalContent>
+              <button onClick={handleSubmit}>click</button>
+              <h1> option</h1>
+              {pics.name}
+              {pics.map((pic, i) => (
+                <Pic
+                  key={pic.id}
+                  pic={pic}
+                  idx={i}
+                  handleShowIndexs={handleShowIndexs}
+                  showPic={showIndexs.indexOf(i) > -1}
+                  modal
+                />
+              ))}
+
+              <CloseModalButton
+                aria-label="Close modal"
+                onClick={() => setShowModal((prev) => !prev)}
+              />
+            </ModalContent>
+          </ModalWrapper>
+        </Background>
+      ) : null}
+    </>
+  );
 };
--- src/components/pic.jsx
+++ src/components/pic.jsx
@@ -1,39 +1,46 @@
-import React, { useState } from 'react'
-import styled from 'styled-components'
+
+import React, { useState } from "react";
+import styled from "styled-components";
 
 const Img = styled.img`
-display: flex;
-flex-direction: row;
-height:100px;
-width: 100px;
-`
+  display: flex;
+  flex-direction: row;
+  height: 100px;
+  width: 100px;
+`;
 const Container = styled.div`
   display: flex;
   flex-wrap: wrap;
   justify-content: space-between;
-`
+`;
 const Wrapper = styled.div`
- display: flex;
+  display: flex;
   flex-direction: column;
   align-items: center;
   width: 300%;
   margin: 0 0 1em 0;
   padding: 1em;
-  border: 1px solid black;`
+  border: 1px solid black;
+`;
 
 export default function Pic({ pic, idx, showPic, handleShowIndexs, modal }) {
-return (
-<Container>
-    <Wrapper>
-        {pic.name}
-<div style={showPic ? { display: "flex" } : { display: "none" }}>
-<Img
-src={pic.img}
-
-/>
-</div>
-{(modal) && <button onClick={() => handleShowIndexs(idx, showPic ? false : true)}>{showPic ? 'Hide' : 'Show'}</button>}
-</Wrapper>
-</Container>
-)
+    return (
+        <>
+    <Container>
+      
+        
+        <Wrapper style={showPic ? { display: "flex" } : { display: "none" }}>
+            {pic.name}
+          <Img src={pic.img} />
+          </Wrapper>
+       
+        { (modal) &&  <button onClick={() => handleShowIndexs(idx, showPic ? false : true)}>{showPic ? 'Hide' : 'Show'}</button>  }
+        
+        
+        
+    </Container>
+     
+    </>
+  );
 }
+            
